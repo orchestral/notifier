966ed842b2e689d6b790441ca82194474d43dde5
crynobone <crynobone@gmail.com>
2013-12-16 02:02:34 +0800

    Initial commit.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Notifier/NotifierInterface.php
@@ -0,0 +1,18 @@
+<?php namespace Orchestra\Notifier;
+
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+interface NotifierInterface
+{
+    /**
+     * Send notification via API.
+     *
+     * @param  \Illuminate\Auth\Reminders\RemindableInterface  $user
+     * @param  string                                          $subject
+     * @param  string                                          $view
+     * @param  array                                           $data
+     * @return boolean
+     */
+    public function send(RemindableInterface $user, $subject, $view, array $data = array());
+}
