a858009f9cad03e5c6f6738bd4ea311ae2f26c4d
crynobone <crynobone@gmail.com>
2013-12-18 13:52:09 +0800

    Add basic service provider.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Notifier/NotifierServiceProvider.php
@@ -0,0 +1,67 @@
+<?php namespace Orchestra\Notifer;
+
+use Illuminate\Support\ServiceProvider;
+
+class NotifierServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var boolean
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerMailer();
+    }
+
+    /**
+    * Register the service provider for mail.
+    *
+    * @return void
+    */
+    protected function registerMailer()
+    {
+        $this->app->bindShared('orchestra.mail', function ($app) {
+            return new Mailer($app);
+        });
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerMemoryEvents();
+    }
+
+    /**
+     * Register memory events during booting.
+     *
+     * @return void
+     */
+    protected function registerMemoryEvent()
+    {
+        $memory = $app['orchestra.memory']->makeOrFallback();
+
+        $this->app['orchestra.mail']->attach($memory);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('orchestra.mail');
+    }
+}
