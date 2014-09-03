be148ee7a0e3c0b2a2d85b063e0115d189e708f4
crynobone <crynobone@gmail.com>
2014-09-04 01:08:11 +0800

    Convert transport to use manager.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Notifier/TransportManager.php
@@ -0,0 +1,114 @@
+<?php namespace Orchestra\Notifier;
+
+use Illuminate\Support\Manager;
+use Illuminate\Mail\Transport\LogTransport;
+use Illuminate\Mail\Transport\MailgunTransport;
+use Illuminate\Mail\Transport\MandrillTransport;
+use Orchestra\Memory\ContainerTrait;
+use Swift_Transport;
+use Swift_SmtpTransport as SmtpTransport;
+use Swift_MailTransport as MailTransport;
+use Swift_SendmailTransport as SendmailTransport;
+
+
+class TransportManager extends Manager
+{
+    use ContainerTrait;
+
+    /**
+     * Register the SMTP Swift Transport instance.
+     *
+     * @return \Swift_Transport
+     */
+    protected function createSmtpDriver()
+    {
+        $config = $this->getTransportConfig();
+
+        $transport = SmtpTransport::newInstance($config['host'], $config['port']);
+
+        if (isset($config['encryption'])) {
+            $transport->setEncryption($config['encryption']);
+        }
+
+        // Once we have the transport we will check for the presence of a username
+        // and password. If we have it we will set the credentials on the Swift
+        // transporter instance so that we'll properly authenticate delivery.
+        if (isset($config['username'])) {
+            $transport->setUsername($config['username']);
+            $transport->setPassword($config['password']);
+        }
+
+        return $transport;
+    }
+
+    /**
+     * Register the Sendmail Swift Transport instance.
+     *
+     * @return \Swift_Transport
+     */
+    protected function createSendmailDriver()
+    {
+        $config = $this->getTransportConfig();
+
+        return SendmailTransport::newInstance($config['sendmail']);
+    }
+
+    /**
+     * Register the Mail Swift Transport instance.
+     *
+     * @return \Swift_Transport
+     */
+    protected function createMailDriver()
+    {
+        return MailTransport::newInstance();
+    }
+
+    /**
+     * Register the Mailgun Swift Transport instance.
+     *
+     * @return \Swift_Transport
+     */
+    protected function createMailgunDriver()
+    {
+        $config = $this->getTransportConfig();
+
+        return new MailgunTransport($config['secret'], $config['domain']);
+    }
+
+    /**
+     * Register the Mandrill Swift Transport instance.
+     *
+     * @return \Swift_Transport
+     */
+    protected function createMandrillDriver()
+    {
+        $config = $this->getTransportConfig();
+
+        return new MandrillTransport($config['secret']);
+    }
+
+    /**
+     * Register the "Log" Swift Transport instance.
+     *
+     * @return \Swift_Transport
+     */
+    protected function createLogDriver()
+    {
+        return new LogTransport($this->app['log']->getMonolog());
+    }
+
+    protected function getTransportConfig()
+    {
+        return $this->memory->get('email');
+    }
+
+    /**
+     * Get the default driver name.
+     *
+     * @return string
+     */
+    public function getDefaultDriver()
+    {
+        return $this->memory->get('email.driver', 'mail');
+    }
+}
